2555852b40b74278554aef757509b8692ef0be23
Filippo Giorgio Rondò <filippogiorgior@gmail.com>
2025-04-01 06:53:22 +0200

    Update es5_condizioni.py

--- 31 Marzo - Condizioni e Cicli/Es Ripasso Condizioni/es5_condizioni.py
+++ 31 Marzo - Condizioni e Cicli/Es Ripasso Condizioni/es5_condizioni.py
@@ -1,10 +1,10 @@
-#input trasformato in numero intero per verificare l'età
+# Input trasformato in numero intero per verificare l'età
 eta = int(input("Benvenuto! Prima di veder il film, ti chiediamo di inserire la tua età: "))
 
-#blocco condizione per verificare che sia maggiorenne
+# Blocco condizione per verificare che sia maggiorenne
 if eta >= 18:
     print("Perfetto, puoi vedere il film")
 
-#se la condizione precedente non è verificata, viene impedita la visione del film
+# Se la condizione precedente non è verificata, viene impedita la visione del film
 else:
     print("I minorenni non possono vedere il film")
