a221140e341b6357ab2c79551804332312350078
filippogiorgiorondo <filippogiorgior@gmail.com>
2025-04-08 12:28:18 +0200

    Update contaParoleRipetute.py

--- 7-8 Aprile - Esercitazione/Esercizi/contaParoleRipetute.py
+++ 7-8 Aprile - Esercitazione/Esercizi/contaParoleRipetute.py
@@ -1,10 +1,10 @@
-def pulizia(stringa): # rripulisce la stringa
+def pulizia(stringa): # ripulisce la stringa
     for c in caratteri_da_rimuovere: 
         stringa = stringa.replace(c, "")
     stringa = stringa.split()
     return stringa
-    
-def contaParole(stringa): # aggiunge ogni parola ad un dizionario, se è già presente ne incrementa il valore
+
+def contaParole(stringa): # aggiunge ogni parola a un dizionario, se è già presente ne incrementa il valore
     conteggio = {}
     for parola in stringa:
         if parola in conteggio:
@@ -12,12 +12,15 @@ def contaParole(stringa): # aggiunge ogni parola ad un dizionario, se è già pr
         else:
             conteggio[parola] = 1
 
-    duplicato = {k : v for k,v in conteggio.items() if v > 1} # stampa solo le parole che sono ripetute più di una volta
-    for key, value in duplicato.items(): # lavoro con chiave e valore
-        print(f"\nLa parola [{key}] appare [{value}] volte, ed è lunga [{len(key)}]!")
+    duplicato = {k : v for k,v in conteggio.items() if v > 1} # seleziona solo le parole ripetute
+    if duplicato:  # Se il dizionario non è vuoto
+        for key, value in duplicato.items(): 
+            print(f"\nLa parola [{key}] appare [{value}] volte, ed è lunga [{len(key)}]!")
+    else:
+        print("\nNon ci sono parole duplicate.")
 
 caratteri_da_rimuovere = [",", ".", "!", "?"]
 stringa = input("Inserisci la stringa da analizzare: ").lower()
 
-stringa = pulizia(stringa)
+stringa = pulizia(stringa) 
 contaParole(stringa)
